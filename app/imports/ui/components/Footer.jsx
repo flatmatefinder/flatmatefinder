fbd06932039526099b1a47018e054507747f0fd6
Briana Lee <brianall@hawaii.edu>
2023-04-11 09:56:37 -1000

    Issue-19

--- app/imports/ui/components/Footer.jsx
+++ app/imports/ui/components/Footer.jsx
@@ -1,28 +1,29 @@
 import React from 'react';
 import { Row, Col, Container } from 'react-bootstrap';
 
-
 /** The Footer appears at the bottom of every page. Rendered by the App Layout component. */
 const Footer = () => (
   <footer className="mt-auto py-3 bg-dark">
     <Container className="text-light">
       <Row className="text-center">
         <p>
-          By signing up you agree to our <a href="https://www.google.com" target="_top">Terms of Use</a> and  <a href="https://www.google.com" target="_top">Privacy Policy</a>.
-          </p>
+          By signing up you agree to our <a href="/termsandconditions">Terms of Use</a> and <a href="/privacypolicy">Privacy Policy</a>.
+        </p>
       </Row>
       <Row className="py-3 text-center">
-        <Col></Col>
+        <Col />
         <Col>
-          <div>Meet the Team</div>
+          <div>Meet the Team
+
+          </div>
         </Col>
         <Col>
-          <div>Suggestions</div>
+          <a href="/suggestions" id="suggestions">Suggestions</a>
         </Col>
         <Col>
           <div>Github</div>
         </Col>
-        <Col></Col>
+        <Col />
       </Row>
     </Container>
   </footer>
