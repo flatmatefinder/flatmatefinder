bb613cdcc0f70bd853f29df17e25d823a81145e4
Kaysha Kealalio <kayshakk@hawaii.edu>
2023-04-11 09:26:02 -1000

    Insert Flatmate Finder Logo
    Inserted Flatmate Finder Logo in the Navbar

--- app/imports/ui/components/NavBar.jsx
+++ app/imports/ui/components/NavBar.jsx
@@ -3,7 +3,7 @@ import { Meteor } from 'meteor/meteor';
 import { useTracker } from 'meteor/react-meteor-data';
 import { NavLink } from 'react-router-dom';
 import { Roles } from 'meteor/alanning:roles';
-import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
+import { Container, Nav, Navbar, NavDropdown, Image } from 'react-bootstrap';
 import { BoxArrowRight, PersonFill, PersonPlusFill } from 'react-bootstrap-icons';
 
 const NavBar = () => {
@@ -15,6 +15,7 @@ const NavBar = () => {
   return (
     <Navbar bg="light" expand="lg">
       <Container>
+        <Navbar.Brand href="#home"><Image src="FlatmateLogo.png" width={80} /></Navbar.Brand>
         <Navbar.Brand as={NavLink} to="/">
           <h2>Flatmate Finder</h2>
         </Navbar.Brand>
