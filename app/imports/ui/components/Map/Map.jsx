6a55125a516b6a1741f77c9785ec2708d2c6143d
Briana Lee <brianall@hawaii.edu>
2023-04-18 13:21:11 -1000

    Issue-11

---
+++ app/imports/ui/components/Map/Map.jsx
@@ -0,0 +1,99 @@
+import React from 'react';
+import { InfoWindow, Marker, GoogleMap } from '@react-google-maps/api';
+
+import PropTypes from 'prop-types';
+
+const containerStyle = {
+  width: '100%',
+  height: '88.5%',
+  position: 'absolute',
+  marginTop: '0px',
+};
+
+const zoom = 11;
+
+const mapOptions = {
+  mapTypeControl: false,
+  minZoom: zoom,
+  restriction: {
+    latLngBounds: {
+      north: 30,
+      south: 10,
+      east: -100,
+      west: 200,
+    },
+  },
+};
+
+const center = {
+  lat: 21.483254,
+  lng: -158.097018,
+};
+
+class Map extends React.Component {
+
+  constructor() {
+    super();
+    this.state = {
+      isOpen: false, // Hides or shows the InfoWindow
+      activePosition: {},
+      selectedUser: {},
+    };
+  }
+
+  onMarkerClick = (user) => this.setState({
+    selectedUser: user,
+    activePosition: user.position,
+    isOpen: true,
+  });
+
+  onClose = () => {
+    if (this.state.isOpen) {
+      this.setState({
+        isOpen: false,
+      });
+    }
+  };
+
+  render() {
+    const users = this.props.users;
+    return (
+      <GoogleMap
+        mapContainerStyle={containerStyle}
+        center={center}
+        zoom={11}
+        options={mapOptions}
+      >
+        {
+          users.map((user, index) => (
+            <Marker
+              key={index}
+              position={user.position}
+              icon="./images/personIcon.png"
+              clickable
+              onClick={() => this.onMarkerClick(user)}
+            />
+          ))
+        }
+        {this.state.isOpen && (
+          <InfoWindow
+            position={this.state.activePosition}
+            onCloseClick={this.onClose}
+            options={{ pixelOffset: { height: -30, width: 0 } }}
+          >
+            <div className="scrollFix">
+              <MapPin selectedUser={this.state.selectedUser} currentUser={this.props.currentUser} />
+            </div>
+          </InfoWindow>
+        )}
+      </GoogleMap>
+    );
+  }
+}
+
+Map.propTypes = {
+  users: PropTypes.array.isRequired,
+  currentUser: PropTypes.object.isRequired,
+};
+
+export default Map;
---
+++ app/imports/ui/components/Map/MapPin.jsx
@@ -0,0 +1,75 @@
+import React from 'react';
+import { Meteor } from 'meteor/meteor';
+import { withTracker } from 'meteor/react-meteor-data';
+import { Loader, Grid, GridColumn } from 'semantic-ui-react';
+import PropTypes from 'prop-types';
+import MapPinUserInfo from './MapPinUserInfo';
+import MapPinSchedule from './MapPinSchedule';
+import CreateReport from '../UserProfile/CreateReport';
+import { Conversations } from '../../../api/conversation/Conversations';
+import Conversation from '../Messages/Conversation';
+
+/** Displays clickable markers placed at users' locations
+ *  Shows user information when clicked */
+class MapPin extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      open: false,
+      createNew: [],
+    };
+  }
+
+  render() {
+    return (this.props.ready) ? this.renderPage() : <Loader active>Getting data</Loader>;
+  }
+
+  renderPage() {
+    const selectedUser = this.props.selectedUser;
+    const currentUser = this.props.currentUser;
+    const conversations = this.props.conversations.filter((conversation) => (conversation.users.some((user) => (user === selectedUser.owner)))).filter((conversation) => (conversation.users.length === 2));
+    let messageButton;
+    let reportButton;
+    if (selectedUser.owner === currentUser.owner) {
+      messageButton = <></>;
+      reportButton = <></>;
+    } else {
+      messageButton = <Conversation currentUser={currentUser} conversations={conversations} selectedUser={selectedUser} />;
+      reportButton = <CreateReport reportedUser={selectedUser.owner} conversations={this.props.conversations} currentUser={currentUser.owner} />;
+    }
+    return (
+      <div width={9}>
+        <MapPinUserInfo user={selectedUser} />
+        <MapPinSchedule trips={selectedUser.trips} />
+        <Grid columns={2}>
+          <GridColumn textAlign="left">
+            {reportButton}
+          </GridColumn>
+          <GridColumn textAlign="right">
+            {messageButton}
+          </GridColumn>
+        </Grid>
+      </div>
+    );
+  }
+}
+
+MapPin.propTypes = {
+  selectedUser: PropTypes.object.isRequired,
+  currentUser: PropTypes.object.isRequired,
+  conversations: PropTypes.array.isRequired,
+  ready: PropTypes.bool.isRequired,
+};
+
+export default withTracker(() => {
+  // Get access to Stuff documents.
+  const subscription = Meteor.subscribe(Conversations.userPublicationName);
+  // Determine if the subscription is ready
+  const ready = subscription.ready();
+  // Get the Stuff documents
+  const conversations = Conversations.collection.find({}).fetch();
+  return {
+    conversations,
+    ready,
+  };
+})(MapPin);
