2d5f289f8596b9853499779f2ddc448db203d97d
Briana Lee <brianall@hawaii.edu>
2023-04-25 10:14:42 -1000

    Issue-26

--- app/imports/ui/pages/Suggestions.jsx
+++ app/imports/ui/pages/Suggestions.jsx
@@ -9,6 +9,17 @@ const Suggestions = () => (
         <h2>Suggestions</h2>
       </Col>
     </Row>
+    <Row>
+      <iframe
+        src="https://docs.google.com/forms/d/e/1FAIpQLSdTvYuRCeATWPROCT1r4I3DlaZWd4-5DsOE4T1yLT9QGjzQWg/viewform?embedded=true"
+        width="675"
+        height="444"
+        frameBorder="0"
+        marginHeight="0"
+        marginWidth="0"
+      >Loading…
+      </iframe>
+    </Row>
   </Container>
 );
 
